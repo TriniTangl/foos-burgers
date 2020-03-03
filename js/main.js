e404b15b453860dee82a79e2f4a675ecc2f6be92
TriniTangl <vlad.tron.27031998@gmail.com>
2020-03-03 19:46:39 +0200

    Revert "add modal windows && refactor CSS, HTML and JS code && fix js script and add functions for modal windows"
    This reverts commit 6ca37fdc305ddbf4c303d8d9055bc8b74b9a627d.

--- js/main.js
+++ js/main.js
@@ -1,68 +1,43 @@
 window.onload = () => {
     document.querySelectorAll('.tab-group').forEach(group => {
         group.querySelectorAll('.tab-switch > a').forEach(link => {
-            link.addEventListener('click', evt => selectElement(evt, 'tab-switch', 'tab-content'));
+            link.addEventListener('click', selectTab);
         });
     });
 
     document.querySelectorAll('.category-group').forEach(group => {
         group.querySelectorAll('.category-switch > a').forEach(link => {
-            link.addEventListener('click', evt => selectElement(evt, 'category-switch', 'category-content'));
+            link.addEventListener('click', selectCategory);
         });
     });
-
-    document.querySelectorAll('.overlay').forEach(overlay => {
-        overlay.addEventListener('click', evt => closeByOverlay(evt))
-    });
-
-    document.querySelectorAll('.overlay > .modal > button.close').forEach(button => {
-        button.addEventListener('click', evt => closeModalWindow(evt))
-    });
-
-    document.querySelectorAll('.overlay > .modal > .modal-footer > button.cancel').forEach(button => {
-        button.addEventListener('click', evt => closeModalWindow(evt))
-    });
-
-    document.getElementById('open-custom-modal').addEventListener('click', evt => {
-        document.querySelector('.modal.custom-modal').parentElement.classList.add('active');
-    });
-
-    document.getElementById('open-small-modal').addEventListener('click', evt => {
-        document.querySelector('.modal.small-modal').parentElement.classList.add('active');
-    });
-
 };
 
-function selectElement(event, nameOfSwitch, containerName) {
+function selectTab(event) {
     if (!event.target.classList.contains('active')) {
-        const links = event.target.parentElement.parentElement.querySelectorAll(`.${ nameOfSwitch } > a`);
-        const contents = event.target.parentElement.parentElement.querySelectorAll(`.${ containerName }`);
-        let indexOfSelect = 0;
-
-        links.forEach((link, index) => {
-            if (link.innerText === event.target.innerText) {
-                indexOfSelect = index;
-            }
-        });
-        links.forEach(link => link.classList.remove('active'));
-        contents.forEach(content => content.classList.remove('active'));
-        links[indexOfSelect].classList.add('active');
-        contents[indexOfSelect].classList.add('active');
+        const links = event.target.parentElement.parentElement.querySelectorAll('.tab-switch > a');
+        const contents = event.target.parentElement.parentElement.querySelectorAll('.tab-content');
+        setActiveClasses(links, contents, event.target.innerText);
     }
 }
 
-function closeModalWindow(event) {
-    let activeOverlay = event.target;
-
-    while (!activeOverlay.classList.contains('overlay') && !activeOverlay.classList.contains('active')) {
-        activeOverlay = activeOverlay.parentElement;
+function selectCategory(event) {
+    if (!event.target.classList.contains('active')) {
+        const links = event.target.parentElement.parentElement.querySelectorAll('.category-switch > a');
+        const contents = event.target.parentElement.parentElement.querySelectorAll('.category-card');
+        setActiveClasses(links, contents, event.target.innerText);
     }
-
-    activeOverlay.classList.remove('active');
 }
 
-function closeByOverlay(event) {
-    if (event.target.classList.contains('overlay') && event.target.classList.contains('active')) {
-        event.target.classList.remove('active');
-    }
+function setActiveClasses(links, contents, innerText) {
+    let indexOfSelect = 0;
+    links.forEach((link, index) => {
+        if (link.innerText === innerText) {
+            indexOfSelect = index;
+        }
+    });
+
+    links.forEach(link => link.classList.remove('active'));
+    contents.forEach(content => content.classList.remove('active'));
+    links[indexOfSelect].classList.add('active');
+    contents[indexOfSelect].classList.add('active');
 }
